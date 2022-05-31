13494a80e6493b11f3c6bed6db0eed667aa41828
robin <robinnsheen@berkeley.edu>
2022-05-31 16:39:49 -0700

    added function docstrings

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 "use strict";
 
+/* prevent refreshing, add a gif from gify api, and append to DOM*/
 async function submitHandler(evt) {
   evt.preventDefault();
+
   const nameVal = $("#name").val();
   let gif = await axios.get("http://api.giphy.com/v1/gifs/search",
     { params: { "api_key": "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym", "q": nameVal } });
+
   console.log(gif.data);
   console.log(gif.data.data);
 
@@ -19,7 +22,7 @@ async function submitHandler(evt) {
 $("#submit").on("click", submitHandler);
 $("#remove").on("click", removeGif);
 
-
+/* prevent refreshing, remove all gifs from gifs div*/
 function removeGif(evt) {
   evt.preventDefault();
   $("#gifs").empty();

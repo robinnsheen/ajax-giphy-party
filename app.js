f0d5ac35f83ecf2dd64e9691bd5ce8f2bb5f7e01
Jordan Asano <jordanasano@yahoo.com>
2022-05-31 16:25:47 -0700

    added div for gifs to in and logic to make AJAX call and add the data into the div

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ async function submitHandler(evt) {
   evt.preventDefault();
   const nameVal = $("#name").val();
   let gif = await axios.get("http://api.giphy.com/v1/gifs/search",
-                            {params: {"api_key":"MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym", "q": nameVal}});
+    { params: { "api_key": "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym", "q": nameVal } });
   console.log(gif.data);
+  console.log(gif.data.data);
 
+  const gifUrl = gif.data.data[0].images.original.url;
+  const gifHtml = `<img src="${gifUrl}"/>`;
+
+  console.log(gifUrl);
+
+  $('#gifs').append($(gifHtml));
 }
 
 $("#submit").on("click", submitHandler);

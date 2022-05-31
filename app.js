933c1138cdf119b7d0b6a1794d31cc3467383b88
robin <robinnsheen@berkeley.edu>
2022-05-31 15:56:53 -0700

    created form, created axios.get

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Let's get this party started!");
+"use strict";
+
+async function submitHandler(evt) {
+  evt.preventDefault();
+  const nameVal = $("#name").val();
+  let gif = await axios.get("http://api.giphy.com/v1/gifs/search",
+                            {params: {"api_key":"MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym", "q": nameVal}});
+  console.log(gif.data);
+
+}
+
+$("#submit").on("click", submitHandler);

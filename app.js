dc03bbb5480a9ae04d7877decc63f01986b585ec
Jordan Asano <jordanasano@yahoo.com>
2022-05-31 17:04:33 -0700

    refactored to break function into multiple functions

--- app.js
+++ app.js
@@ -1,29 +1,42 @@
 "use strict";
 
+const API_KEY = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym";
 /* prevent refreshing, add a gif from gify api, and append to DOM*/
 async function submitHandler(evt) {
   evt.preventDefault();
 
-  const nameVal = $("#name").val();
-  let gif = await axios.get("http://api.giphy.com/v1/gifs/search",
-    { params: { "api_key": "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym", "q": nameVal } });
+  let gif = await getJson();
 
   console.log(gif.data);
   console.log(gif.data.data);
 
-  const gifUrl = gif.data.data[0].images.original.url;
-  const gifHtml = `<img src="${gifUrl}"/>`;
-
-  console.log(gifUrl);
+  let gifHtml = createHtml(gif);
 
   $('#gifs').append($(gifHtml));
 }
 
-$("#submit").on("click", submitHandler);
-$("#remove").on("click", removeGif);
+/** Get's the input value, returns JSON object with the input value searched from giphy.com */
+async function getJson() {
+  const q = $("#name").val();
+  let gif = await axios.get("http://api.giphy.com/v1/gifs/search",
+    { params: { "api_key": API_KEY, q } });
+  return gif;
+}
+
+/** Takes in a json object, gets the url for the gif, returns html in an img tag */
+function createHtml(json) {
+  console.log(json);
+  const gifUrl = json.data.data[0].images.original.url;
+  const gifHtml = `<img src="${gifUrl}"/>`;
+  console.log(gifHtml);
+  return gifHtml;
+}
 
 /* prevent refreshing, remove all gifs from gifs div*/
 function removeGif(evt) {
   evt.preventDefault();
   $("#gifs").empty();
 }
+
+$("#submit").on("click", submitHandler);
+$("#remove").on("click", removeGif);
